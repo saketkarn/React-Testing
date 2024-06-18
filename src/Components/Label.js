129d669149837242b1aafab3a1a68076e73533c9
Saket Karn <saketkarn38@gmail.com>
2024-06-18 11:22:16 +0530

    testing of the checkbox

--- src/Components/Label.js
+++ src/Components/Label.js
@@ -3,11 +3,21 @@ import React from 'react'
 const Label = () => {
   return (
     <div>
-        <h1>Testing of getByLabelText</h1>
-        <label htmlFor='username'>Username</label>
-        <input type="text" id="username"/>
+        <div>
+          <h1>Testing of getByLabelText</h1>
+          <label htmlFor='username'>Username</label>
+          <input type="text" id="username" defaultValue={"Saket"}/>
+        </div>
+        <div>
+          <h1>Testing of getByLabelText</h1>
+          <label htmlFor='skills'>Skills</label>
+          <input type="checkbox" id="skills"/>
+        </div>
     </div>
   )
 }
 
 export default Label
+
+
+//in the 7th line, using htmlFor, we associated the label with the input tag.
--- src/__tests__/Label.test.js
+++ src/__tests__/Label.test.js
@@ -2,8 +2,15 @@ import { render, screen } from "@testing-library/react"
 import Label from "../Components/Label.js"
 import "@testing-library/jest-dom"
 
-test("getByLabelText",()=>{
+test("getByLabelText test case 1",()=>{
     render(<Label/>)
     const input=screen.getByLabelText("Username")
     expect(input).toBeInTheDocument()
+    expect(input).toHaveValue("Saket")
+})
+
+test("getByLabelText test case 2",()=>{
+    render(<Label/>)
+    const checkbox=screen.getByLabelText("Skills")
+    expect(checkbox).toBeInTheDocument()
 })
