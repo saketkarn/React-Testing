461c6cff2a2394a873e06d6845b115213e2b9207
Saket Karn <saketkarn38@gmail.com>
2024-06-18 11:14:47 +0530

    same

--- src/Components/Label.js
+++ src/Components/Label.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Label = () => {
   return (
     <div>
+        <h1>Testing of getByLabelText</h1>
         <label htmlFor='username'>Username</label>
         <input type="text" id="username"/>
     </div>
